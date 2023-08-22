6a5872bb7e0e5f29ff8ae0b0934226a793e772dd
Zechariah Janowicz <zechariahjanowicz@gmail.com>
2023-08-22 18:01:12 -0500

    Fixed up javascript so that the buttons log the scores properly by creating individual functions that then will end up summing the score up at the very end of the game.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,11 @@
 var playerTally = 0;
 var computerTally = 0;
-
+var answerRock = "rock"
 
 /*Rock = 1 Scissors = 2 Paper = 3 */
 
-let playerChoice = () => selection = prompt("Rock Paper or Scissors: ");
+
+
 
 let computerChoice = () => compSelection = Math.floor(Math.random() *3 + 1);
 
@@ -17,54 +18,78 @@ function computerTranslated(){
         compSelection = "Scissors";
     else if (compSelection = 3)
         compSelection = "Paper";
+    }
 
-}
+    let rockString = () => answerRock = "rock";
+    let paperString = () => answerPaper = "paper";
+    let scissorString = () => answerScissor = "scissor";
+    
+    rockString();
+    paperString();
+    scissorString();
+    
+    
+    function paper() {
+        let pA = answerPaper;
+        computerChoice();
+        if (pA && compSelection == 1) {
+            console.log("You win! Computer chose Rock!");
+            ++playerTally;}
+        else if (pA && compSelection == 3) {
+            console.log("You Drew! Computer chose Paper!");}
+        else if (pA && compSelection == 2) {
+            console.log("You lose! Computer chose Scissors!");
+            ++computerTally;
+            }
+            console.log(`${playerTally} and ${computerTally}`)
+        }
+    
+    function rock() {
+        let rA = answerRock;
+        computerChoice();
+        if ( rA && compSelection == 2) {
+            console.log("You win! Computer chose Scissors!");
+            ++playerTally;}
+        else if (rA  && compSelection == 1) {
+            console.log("You Drew! Computer chose Rock!");}
+        else if (rA && compSelection == 3) {
+            console.log("You lose! Computer chose Paper!");
+            ++computerTally;
+            }
+            console.log(`${playerTally} and ${computerTally}`)
 
-function game() {
-playerChoice();
-computerChoice();
-if (selection.toLowerCase() === 'rock'&& compSelection == 2) {
-    console.log(compSelection)
-    console.log("You win! Computer chose Scissors!");
-    ++playerTally;
-}
-else if (selection.toLowerCase() === 'scissors' && compSelection == 3) {
-    console.log("You win! Computer chose Paper!");
-    ++playerTally;
-}
-else if (selection.toLowerCase() === 'paper' && compSelection == 1) {
-    console.log("You win! Computer chose Rock!");
-    ++playerTally;
-}
-else if (selection.toLowerCase() === 'paper' && compSelection == 3) {
-    console.log("You Drew! Computer chose Paper!");
-}
-else if (selection.toLowerCase() === 'Scissors' && compSelection == 2) {
-    console.log(compSelection)
-    console.log("You Drew! Computer chose Scissors!");
-}
-else if (selection.toLowerCase() === 'rock' && compSelection == 1) {
-    console.log("You Drew! Computer chose Rock!");
-}
-else if (selection.toLowerCase() === 'rock' && compSelection == 3) {
-    console.log("You lose! Computer chose Paper!");
-    ++computerTally;
-}
-else if (selection.toLowerCase() === 'paper' && compSelection == 2) {
-    console.log("You lose! Computer chose Scissors!");
-    ++computerTally;
-}
-else {
-    console.log(compSelection)
-    computerTranslated();
-    console.log(`You lose! Computer chose ${compSelection}`)
-    ++computerTally
-    return playerTally + computerTally
-}
-}
+        }
+    
+    function scissors() {
+        console.log(`${playerTally} and ${computerTally}`)
+        computerChoice();
+        if (answerScissor && compSelection == 3) {
+            console.log("You win! Computer chose Paper!");
+            ++playerTally;
+        }
+        else if (answerScissor && compSelection == 2) {
+            console.log("You Drew! Computer chose Scissors!");
+        }
+        else {
+            console.log(`You lose! Computer chose Rock`)
+            ++computerTally
+            }
+            console.log(`${playerTally} and ${computerTally}`)
+        }
+
+    
 
 
 
+
+
+
+
+
+
+
+
+/*
 while (playerTally < 5 && computerTally < 5) {
     game();
     console.log(`Your score is ${playerTally}`)
@@ -76,3 +101,4 @@ while (playerTally < 5 && computerTally < 5) {
         console.log("Oh no! You lost to the computer!")
     }
 }
+*/
